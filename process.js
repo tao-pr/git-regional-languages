f89c71e07d38450c1d0e7ea1fd3a1ae44416a466
starcolon <tao@starcolon.com>
2016-04-03 01:40:09 +0700

    {PROCESS} Draft geolocation updating process

--- process.js
+++ process.js
@@ -11,7 +11,17 @@
 var colors    = require('colors');
 var _         = require('underscore');
 var MapReduce = require('./mongo/mapreduce.js');
+var Gmap      = require('./mapapi/gmap.js');
 
+const invalid_locations = [
+		'undefined','null','0','1','Earth',
+		'the internet','the interweb','worldwide'
+	]
+
+
+/**
+ * Main entry
+ */
 function prep(){
 	console.log('******************'.green);
 	console.log('  Preparing data'.green)
@@ -25,11 +35,7 @@ function prep(){
 			
 			// Remove invalid language object
 			var _dist = dist.filter((language) => language._id != 'message')
-			
-			var invalid_locations = [
-				'undefined','null','0','1','Earth',
-				'the internet','the interweb','worldwide'
-			]
+
 			_dist = _dist.map(function(language){
 				var _v = Object.keys(language.value)
 					.filter((location) => 
@@ -52,20 +58,51 @@ function prep(){
 			return _dist
 		})
 		.then(function (dist){
-			// TAOTODO: Get geolocation of associated addresses
-			var addresses = {}
-
+			// Pump all regions to another collection
 			return MapReduce.allRegions(datasource)
 				.then(function(regions){
 
-					console.log(regions); // TAODEBUG:
+					// Strip only unique locations
+					var locations = _.uniq(_.pluck(regions,'_id'));
+					console.log(locations); // TAODEBUG:
 
+					return locations;					
+				})
+				.then(function(locations){
+					
+					function createGeoUpdater(location){
+						return Promise.delay(1200)
+							.then(() => updateGeolocation(location))
+					}
+
+					// Delayed update the locations
+					return Promise.each(locations.map(createGeoUpdater))
+						.then((results) => {
+							// TAOTODO: Do something with the results
+
+
+
+						})
 				})
 
 		})
 		.then(() => process.exit(0))
 }
 
+/**
+ * From the given physical location,
+ * update the geolocation to the collection "geo"
+ * @param {String} location/address
+ */
+function updateGeolocation(location){
+	if (!!~invalid_locations.indexOf(location)){
+		console.log(`Skip - ${location}`.yellow);
+		return null;
+	}
+
+	console.log('Updating geolocation: '.cyan, location)
+	return Gmap.locationToInfo(location)
+}
 
 
 // Start
