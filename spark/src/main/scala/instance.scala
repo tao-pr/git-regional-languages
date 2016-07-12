68a17390bfcb3ea30c01266daf84fa1d6e80fc4f
starcolon <tao@starcolon.com>
2016-07-12 17:41:15 +0700

    Spark : Add initial instance module

---
+++ spark/src/main/scala/instance.scala
@@ -0,0 +1,11 @@
+package gitlang
+
+object Core extends App {
+  import scala.concurrent.Await
+  import scala.concurrent.duration._
+  import scala.concurrent.ExecutionContext.Implicits.global
+  import scala.io.StdIn.{ readLine, readInt }
+
+  // TAOTODO:
+
+}
