adf2eb09ca07775a55f5948db137472d2b8f4ec2
starcolon <tao@starcolon.com>
2016-07-17 18:26:55 +0700

    Spark : (WIP) Draft distribution aggregation

--- spark/src/main/scala/analysis.scala
+++ spark/src/main/scala/analysis.scala
@@ -10,18 +10,35 @@ object Analysis {
    * Accumulate the entire distribution map of the code contribution
    * by all languages in the current data frame and create a mapping
    * which maps: [Exact geospatial location of the centroid] => [Amount of code contribution in total]
-   * @param {SQLContext} 
-   * @param {DataFrame}
+   * @param {SQLContext}
+   * @param {DataFrame} Filtered distribution dataset
    * @return {Map} [SpatialLocation] => [Float]
    */
-  def accumAllDists(sqlctx: SQLContext, dists: DataFrame): Map[SpatialLocation,Float] = {
+  def accumGlobalDists(sqlctx: SQLContext, dists: DataFrame): Map[SpatialLocation, Float] = {
     import sqlctx.implicits._
-    val contrib = dists.select($"coords")
+    val contrib = dists.select($"coords").collect()
 
     // Create a mapping where geolocation plays as keys
-    val map = Map.empty[SpatialLocation,Float]
+    val map = Map.empty[SpatialLocation, Float]
 
-    // TAOTODO:
+    // TAOTODO: Consider using `groupBy`?
+    contrib.foreach { (dataArray) =>
+      val lat = dataArray(0).asInstanceOf[Double]
+      val lng = dataArray(1).asInstanceOf[Double]
+      val density = dataArray(2).asInstanceOf[Float]
+
+      val loc = SpatialLocation(lat, lng)
+      map(loc) = density + map(loc)
+    }
     map
+  }
+
+  /**
+   * Compute a bin vector from the given geospatial distribution data
+   * @param {SQLContext}
+   * @param {DataFrame} current distribution data
+   */
+  def geodistToBins(sqlctx: SQLContext, dists: DataFrame): Map[Int, Float] = {
+    // TAOTODO:
   }
 }
